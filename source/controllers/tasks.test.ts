89d9e66f447530e28a85c9480058a77227d8adb4
Pedro Rodrigues <pedrovitorlima@gmail.com>
2023-01-28 16:14:22 +1100

    Handle errors with param is_processed in the GET endpoint

--- source/controllers/tasks.test.ts
+++ source/controllers/tasks.test.ts
@@ -16,15 +16,15 @@ describe('The Water Pump task controllers', () => {
         });
     }
 
-    const emptyRequest = {  body: {} } as Request
+    const emptyRequest = {  body: {}, query: {} } as Request
+
 
     const queryShouldFail = () => {
         (pool as any).query.mockRejectedValue();
     }
 
-    describe('When getTask is called with tasks in the database with no parameters', () => {
-
-        it('should return the tasks', async () => {
+    describe('When getTask is called with no params specified', () => {
+        test('then it should return the tasks', async () => {
             const tasks = [{
                 id: 0,
                 duration: 1.0,
@@ -40,50 +40,77 @@ describe('The Water Pump task controllers', () => {
             expect(res.responseObject.body).toEqual(tasks)
             expect((pool as any).release).toHaveBeenCalled()
         })
+
+         describe('when the database connection fails', () => {
+            test('than it should log the error', async () => {
+                queryShouldFail()
+                console.log = jest.fn();
+    
+                await getTasks(emptyRequest, new ResponseMock().mockResponse(), jest.fn())
+    
+                expect(console.log).toHaveBeenCalled();
+            })
+         })
     })
 
-    describe('When getTask is called with tasks in the database and parameter pending filled', () => {
-
-        it ('should use specific query', async () => {
+    describe('when getTask is called with parameters', () => {
+        test ('then it should use specific a query for that', async () => {
             queryShouldReturn([])
-
+    
             var res = new ResponseMock()
-
+    
             const request = {
                 query: {}
             }
-
+    
             request.query = {is_processed: false}
-
+    
             await getTasks(request as Request, res.mockResponse(), jest.fn())
-
+    
             expect((pool as any).query).toHaveBeenCalledWith('SELECT * FROM public.tasks WHERE is_processed = false')
-         })
+        })
 
-    })
+        describe('when is_processed is invalid', () => {
+            test ('then it should fail if is_processed is not a number', async () => {
+                queryShouldReturn([])
+        
+                var responseMock = new ResponseMock()
+                var res = responseMock.mockResponse()
+                const request = {
+                    query: {}
+                }
+        
+                request.query = {is_processed: "invalid" }
+        
+                await getTasks(request as Request, res, jest.fn())
+        
+                expect(responseMock.statusCode).toEqual(400)
+                expect(responseMock.responseObject).toEqual({error: "param is_processed is invalid."})
+            })
+        })
+        
 
-    describe('When an error happen while connecting to the database', () => {
-        it ('should log the error', async () => {
-            queryShouldFail()
-            console.log = jest.fn();
+        test ('then it should fail if any other parameter is provided', () => {
 
-            await getTasks(emptyRequest, new ResponseMock().mockResponse(), jest.fn())
-
-            expect(console.log).toHaveBeenCalled();
         })
     })
 })
 
 class ResponseMock {
     declare responseObject: { body: {} }
+    declare statusCode: 0
 
     mockResponse(): Response  {
         const res: Partial<Response> = {
             json: jest.fn().mockImplementation((result) => {
                 this.responseObject = result
-            }),
-            status: jest.fn().mockReturnThis(),
+            })
         };
+
+        res.status = jest.fn().mockImplementation((status) => {
+            this.statusCode = status
+            return res
+        })
 
         return res as Response
     }
--- source/controllers/tasks.ts
+++ source/controllers/tasks.ts
@@ -1,5 +1,6 @@
 import pool from '../database/dbconfig';
 import { Request, Response, NextFunction } from "express"
+import { ParsedQs } from 'qs';
 
 export interface Task {
     id: Number,
@@ -11,6 +12,16 @@ export const getTasks = async (req: Request, res: Response, next: NextFunction) 
 
     console.log("Connecting with the pull of connections...")
     const client = await pool.connect();
+
+    try {
+        checkParametersValid(req.query)
+    } catch(e:unknown) {
+        console.error(e)
+
+        return res.status(400).json({
+            error: (e as Error).message
+        })
+    }
 
     const isProcessed = req.query.is_processed
 
@@ -31,8 +42,7 @@ export const getTasks = async (req: Request, res: Response, next: NextFunction) 
     return res.status(200).json({
         body: all
     })
-
-}
+}  
 
 const updateTasks = (req: Request, res: Response, next: NextFunction) => {
     let id = req.params.id
@@ -52,3 +62,17 @@ const updateTasks = (req: Request, res: Response, next: NextFunction) => {
 }
 
 export default { getTasks, updateTasks }
+
+function checkParametersValid(query: ParsedQs) {
+    if (query.is_processed == null) {
+        return
+    }
+    
+    const isProcessedParamNotBoolean =
+        (query.is_processed.toString() != "true" &&
+        query.is_processed.toString() != "false")
+
+    if (isProcessedParamNotBoolean) {
+        throw new Error('param is_processed is invalid.');
+    }
+}
